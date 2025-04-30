43df7fad19fc4972ce5e006eda9308dd04589240
Eugene Martinez <eugenejrmartinez@gmail.com>
2025-04-30 20:25:38 +0800

    .

--- src/views/NoteDetailView.vue
+++ src/views/NoteDetailView.vue
@@ -188,7 +188,7 @@ const handleTagClick = (tag: string) => {
 
         <!-- Responsive Actions Container - Animate -->
         <div
-          class="relative flex-shrink-0"
+          class="relative z-10 flex-shrink-0"
           v-motion
           :initial="{ opacity: 0, x: 10 }"
           :enter="{ opacity: 1, x: 0, transition: { delay: 250 } }"
